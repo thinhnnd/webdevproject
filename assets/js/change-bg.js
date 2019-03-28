210cb3b27edea85fdb93d30ae113dfdf8ffe6770
Thinh NND <16521179@gm.uit.edu.vn>
2019-03-28 19:50:45 +0700

    test deployment on heroku

---
+++ assets/js/change-bg.js
@@ -0,0 +1,30 @@
+
+$(document).ready(() =>{
+    $('#btn-open-menu').click(openNav);
+    var i = 0;
+    setInterval(()=> {
+        if(i===4)
+            i = 0;
+        changeBg(i);
+        i++;
+    }, 5000);
+});
+
+let bgLink = [
+    "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('/assets/img/backgrounds-wraper/bg-02-hoa-sen.jpg') center no-repeat",
+    "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('/assets/img/backgrounds-wraper/bia1_QBQI.jpg') center no-repeat",
+    "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('/assets/img/backgrounds-wraper/hoang-thanh-thang-long-02.jpg') center no-repeat",
+    "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('/assets/img/backgrounds-wraper/bg-01-khue-van-cac.jpg') center no-repeat",
+]
+
+console.log(bgLink);
+
+function changeBg(num){
+        // $('.wraper-bg ').animate("-webkit-animation:","slide 12s linear infinite");
+    $('#wraper-animation').css("background", bgLink[num]);
+    $('#wraper-animation').css("background-size", "cover");
+    $('#wraper-animation').css("transition", "background-image 1s ease-in-out");
+
+
+
+}
