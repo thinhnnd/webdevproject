0e8a6ddf35e6d6785c0d064e4de17a4976bc6da1
Thinh NND <16521179@gm.uit.edu.vn>
2019-03-29 09:43:40 +0700

    reduce size of some  backgrounds

--- assets/js/change-bg.js
+++ assets/js/change-bg.js
@@ -14,7 +14,7 @@ let bgLink = [
     "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('./assets/img/backgrounds-wraper/bg-02-hoa-sen.jpg') center no-repeat",
     "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('./assets/img/backgrounds-wraper/bia1_QBQI.jpg') center no-repeat",
     "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('./assets/img/backgrounds-wraper/hoang-thanh-thang-long-04.jpg') bottom center no-repeat",
-    "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('./assets/img/backgrounds-wraper/bg-01-khue-van-cac-2.png') center no-repeat",
+    "linear-gradient(to bottom right,rgba(0,47,75,0.7),rgba(220,66,37,0.7)), url('./assets/img/backgrounds-wraper/bg-01-khue-van-cac-3.jpg') center no-repeat",
 ]
 
 console.log(bgLink);
